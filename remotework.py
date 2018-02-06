7bc2e7d0cb5f40f9966eee2502f26b45c8de0321
lightwinglc <lightwinglc@163.com>
2018-02-06 19:35:33 +0800

    自动安装go
    使用配置在服务器集群安装go

--- remotework.py
+++ remotework.py
@@ -25,8 +25,7 @@ class WorkConf(object):
             self.hostsmap = config.get('hosts', 'hostsmap')
             self.username = config.get('hosts', 'username')
             self.password = config.get('hosts', 'password')
-            self.scpsourcedir = config.get('work', 'scpsourcedir')
-            self.scptargetdir = config.get('work', 'scptargetdir')
+            self.scpmap = config.get('work', 'scpmap')
             self.worklist = config.get('work', 'worklist')
 
 
@@ -36,8 +35,7 @@ class RemoteWork(object):
         self.hostsmap = eval(config.hostsmap)
         self.username = config.username
         self.password = config.password
-        self.scpsourcedir = config.scpsourcedir
-        self.scptargetdir = config.scptargetdir
+        self.scpmap = eval(config.scpmap)
         self.worklist = eval(config.worklist)
 
     def do_allwork(self):
@@ -47,28 +45,41 @@ class RemoteWork(object):
 
     # 在单台主机上执行工作
     def do_singlework(self, host):
-        print 'begin host %s work:#######################################################'%host
+        print 'begin host %s work:#######################################################' % host
         # 登录
         ssh_client = pxssh.pxssh()
         ssh_client.login(host, self.username, self.password)
 
         # 删除目标目录
-        ssh_client.sendline('rm -rf %s' % self.scptargetdir)
-        ssh_client.prompt()
-        print(ssh_client.before)
+        # ssh_client.sendline('rm -rf %s' % self.scptargetdir)
+        # ssh_client.prompt()
+        # print(ssh_client.before)
 
         # 建目标目录
-        father_path = os.path.abspath(os.path.dirname(self.scptargetdir) + os.path.sep + ".")
-        ssh_client.sendline('mkdir -p %s'%father_path)
-        ssh_client.prompt()
-        print(ssh_client.before)
+        # father_path = os.path.abspath(os.path.dirname(self.scptargetdir) + os.path.sep + ".")
+        # ssh_client.sendline('mkdir -p %s'%father_path)
+        # ssh_client.prompt()
+        # print(ssh_client.before)
 
         # 复制包到目标机器
-        (command_output, exitstatus) = pexpect.run('scp -r %s %s@%s:%s'
-            %(self.scpsourcedir, self.username, host, self.scptargetdir), withexitstatus=1)
-        print command_output
-        if exitstatus != 0:
-            return
+        # (command_output, exitstatus) = pexpect.run('scp -r %s %s@%s:%s'
+        #     %(self.scpsourcedir, self.username, host, self.scptargetdir), withexitstatus=1)
+
+        # 复制文件到目标位置
+        for sourcefile in self.scpmap.keys():
+            # 检查目标目录是否存在，不存在则新建
+            father_path = os.path.abspath(os.path.dirname(self.scpmap[sourcefile]))
+            if father_path != os.path.abspath('/'):
+                ssh_client.sendline('mkdir -p %s' % father_path)
+                ssh_client.prompt()
+                print(ssh_client.before)
+
+            # 进行scp操作
+            (command_output, exitstatus) = pexpect.run('scp %s %s@%s:%s'
+                %(sourcefile, self.username, host, self.scpmap[sourcefile]), withexitstatus=1)
+            print command_output
+            if exitstatus != 0:
+                return
 
         # 执行命令
         for workcmd in self.worklist:
@@ -88,6 +99,7 @@ def main():
     myconf = WorkConf(args.config)
     mywork = RemoteWork(myconf)
     mywork.do_allwork()
+    # mywork.do_singlework("132.252.130.11")
 
 
 if __name__ == '__main__':
